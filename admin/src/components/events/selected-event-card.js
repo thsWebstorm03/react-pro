4c209d72f81dbfc7a19fcb752d6edc48ee0ff4a2
thsWebstorm03 <webstorm03@gmail.com>
2018-09-07 20:28:08 +0300

    add simple dnd flow

--- admin/src/components/events/selected-event-card.js
+++ admin/src/components/events/selected-event-card.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { DropTarget } from 'react-dnd'
 
-function SelectedEventCard({ event }) {
-  return (
-    <div style={{ border: '1px solid black', height: 100 }}>
+function SelectedEventCard({
+  event,
+  connectDropTarget,
+  hovered,
+  canDrop,
+  dragType
+}) {
+  const borderColor = canDrop ? (hovered ? 'green' : 'red') : 'black'
+  return connectDropTarget(
+    <div style={{ border: `1px solid ${borderColor}`, height: 100 }}>
       <h3>{event.title}</h3>
       <div>{event.where}</div>
     </div>
@@ -11,4 +19,22 @@ function SelectedEventCard({ event }) {
 
 SelectedEventCard.propTypes = {}
 
-export default SelectedEventCard
+const spec = {
+  drop(props, monitor) {
+    console.log(
+      '---',
+      'eventId:',
+      props.event.id,
+      'personId:',
+      monitor.getItem().id
+    )
+  }
+}
+
+const collect = (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  hovered: monitor.isOver(),
+  canDrop: monitor.canDrop()
+})
+
+export default DropTarget(['person'], spec, collect)(SelectedEventCard)
